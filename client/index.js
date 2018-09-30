7276e237c243d6434e1dbe6c8c2d41a6bb956f0c
bakhaa <bakhaa101@gmail.com>
2018-10-01 01:07:53 +0500

    configure hot reload

--- client/index.js
+++ client/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { AppContainer } from 'react-hot-loader';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/app';
 
 import configureStore from './store';
 
 const initialState = window.__INITIAL_STATE__ || {};
 const store = configureStore(initialState);
-const renderMethod = !!module.hot ? ReactDOM.render : ReactDOM.hydrate
+const renderMethod = !!module.hot ? ReactDOM.render : ReactDOM.hydrate;
 
-renderMethod((
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-), document.getElementById('root'));
+const render = (Component) => {
+  renderMethod((
+    <AppContainer>
+      <Provider store={store}>
+        <Router>
+          <Component />
+        </Router>
+      </Provider>
+    </AppContainer>
+  ), document.getElementById('root'));
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/app', () => {
+    // eslint-disable-next-line
+    const nextApp = require('./components/app').default;
+    render(nextApp);
+  });
+}
