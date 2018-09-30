2bb04bafe279c2b7de4b888f80be672fd9b70969
bakhaa <bakhaa101@gmail.com>
2018-09-30 11:14:22 +0500

    render react on the server

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  "extends": "airbnb"
+  "extends": "airbnb",
+  "rules": {
+    "react/jsx-filename-extension": 0,
+    "object-curly-newline": 0,
+    "comma-dangle": 0
+  }
 };
--- server/app.js
+++ server/app.js
@@ -1,11 +1,47 @@
 import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 
 const app = express();
+
+function renderHTML({ componentHTML, initialState, metaData, config }) {
+  return `
+      <!DOCTYPE html>
+      <html>
+      <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="shortcut icon" href="/static/favicon.ico"/>
+          <title>Test</title>
+      </head>
+      <body>
+      <div id="react-view">${componentHTML}</div>
+      <script type="application/javascript">
+        window.__CONFIG__ = ${JSON.stringify({ isJSON: true })};
+      </script>
+      </body>
+      </html>
+  `;
+}
+
+const Text = () => (
+  <div>
+    <p>Text</p>
+  </div>
+);
+
+const componentHTML = ReactDOMServer.renderToString(
+  <Text />
+);
+
+const html = renderHTML({
+  componentHTML,
+});
 
 const PORT = process.env.PORT || 3001;
 
 app.use('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(html);
 });
 
 app.listen(PORT, () => {
