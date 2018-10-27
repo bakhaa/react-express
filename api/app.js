dcb0507c72c3043cc7c68dfd6b112a11bb696a40
bakhaa <bakhaa101@gmail.com>
2018-10-27 15:44:01 +0500

    api: add graphQL

--- api/app.js
+++ api/app.js
@@ -1,15 +1,30 @@
 import express from 'express';
 import path from 'path';
+import cookie from 'cookie-parser';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
 const app = express();
 
+app.use(cors({}));
+app.use(cookie());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, '/public')));
 
-const PORT = process.env.NODE_PORT || 3003;
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
 
-app.get('*', (req, res, next) => {
-  res.send('Hello, world from API!...');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+server.applyMiddleware({ app });
+
+const PORT = process.env.NODE_PORT || 3003;
 
 app.listen(PORT, () => {
   console.log(`Server listening on: ${PORT}`); // eslint-disable-line no-console
---
+++ api/resolvers/todo.js
@@ -0,0 +1,16 @@
+const todos = [{ id: 1, text: 'test' }, { id: 2, text: 'test2' }];
+
+export default {
+  Todo: {},
+  Query: {
+    getTodo: (parent, { todoId }, { models }) => todos.find(item => item.id === todoId),
+    allTodos: () => todos,
+  },
+  Mutation: {
+    addTodo: (parent, { text }, { models }) => {
+      const todo = { id: todos.length + 1, text };
+      todos.push(todo);
+      return { ok: true, todo };
+    },
+  },
+};
