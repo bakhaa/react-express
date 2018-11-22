6323c332ef8ac51a14702ef12a0bbf2d3c71656e
Alex Bakh <bakhaa101@gmail.com>
2018-11-22 11:35:29 +0500

    web: fix sign up form

--- web/client/pages/RegisterPage.js
+++ web/client/pages/RegisterPage.js
@@ -201,10 +201,6 @@ export default compose(
       values,
       { props: { history, mutate }, setSubmitting, setStatus, resetForm, setErrors },
     ) => {
-      if (values.email === '' || values.password === '') {
-        setSubmitting(false);
-        return;
-      }
       const response = await mutate({
         variables: {
           email: values.email,
@@ -236,18 +232,5 @@ export default compose(
         .email('Invalid email address')
         .required('Email is required'),
     }),
-    // {
-    //   const errors = {};
-    //   if (!values.email) {
-    //     errors.email = 'Email is required';
-    //   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    //     errors.email = 'Invalid email address';
-    //   }
-
-    //   if (!values.password) {
-    //     errors.password = 'Password is required';
-    //   }
-    //   return errors;
-    // },
   }),
 )(RegisterPage);
